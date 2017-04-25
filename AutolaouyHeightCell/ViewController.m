a746a07344264e0321cbb353ae14f1b4b59c86f9
LEI SISI <lg_ss_long@163.com>
2017-04-25 17:45:12 +0800

    Initial Commit

---
+++ AutolaouyHeightCell/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AutolaouyHeightCell
+//
+//  Created by lss on 2017/4/25.
+//  Copyright © 2017年 lss. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
